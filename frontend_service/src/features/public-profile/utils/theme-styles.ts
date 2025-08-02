7a388245ae039ef6e3d4fe36720cd9a92f3fde85
kkvrivishvili <kote@outlook.com>
2025-08-01 22:19:24 -0300

    todo funcionando antes de nueva gran actualizacion

--- frontend_service/src/features/public-profile/utils/theme-styles.ts
+++ frontend_service/src/features/public-profile/utils/theme-styles.ts
@@ -15,6 +15,7 @@ export const getShadowStyle = (theme: ProfileTheme) => {
   return '0 2px 4px rgba(0,0,0,0.1)';
 };
 
+//a
 export const getGlassEffect = () => ({
   backgroundColor: 'rgba(255, 255, 255, 0.1)',
   backdropFilter: 'blur(10px)',
